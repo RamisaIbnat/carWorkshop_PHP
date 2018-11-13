0010b19c0f7962e07ef02becad1a2be1eaa5e7ab
ramisa-ib <ramisa.ib1@gmail.com>
2018-11-13 11:10:37 +0600

    Data retrieved from myphpadmin

--- admin.php
+++ admin.php
@@ -50,26 +50,5 @@ if (!$con)   /*  check if the connection was actually successful  */
     }
    
    ?>
-  
-  
-
-</table>
-// $query = mysql_query("SELECT * FROM client");
-
-
-// while ($rows = mysql_fetch_array($query)):
-
-//     $name = $rows['name'];
-//     $address = $rows['address']; 
-//     $phone = $rows['phone'];
-//     $email = $rows['email'];
-//     $license = $rows['license'];
-//     $date = $rows['date'];
-//     $engine = $rows['engine'];
-//     $mechanic = $rows['mechanic'];
-
-//    echo "$name<br>$address<br>$email<br>$license<br>$phone<br>$date<br>$engine<br>$mechanic";
-
-//    endwhile;
-</body>
+  </table>
 </html>
