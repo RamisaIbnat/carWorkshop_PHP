ab70e1191d28f7e4de9ed660ee75f81dc387a0bf
ramisa-ib <ramisa.ib1@gmail.com>
2018-11-13 11:05:04 +0600

    Update admin.php

--- admin.php
+++ admin.php
@@ -6,17 +6,54 @@ if (!$con)   /*  check if the connection was actually successful  */
     {
         exit("Could not connect to the database: <br/>" . 
             htmlspecialchars(mysql_error()) );
+           
     }
     else{
-        echo "connected";
+      "Hello";
+      
+    }
+?>
 
-// $username="username";
-// $password="password";
-// $database="your_database";
-// $mysqli = new mysqli("localhost", $username, $password, $database);
-// @mysql_select_db($database) or die( "Unable to select database");
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    
+<table >
 
+<tr>
+  <th> User Name</th>
+  <th> Phone Number </th>
+  <th> Car registration Number </th>
+  <th> Appointment Date </th>
+  <th> Mechanic Name </th>
+  </tr> 
 
+   <?php 
+    $q = "SELECT * from client";
+    $r = mysqli_query($con,$q);
+    while($row = mysqli_fetch_array($r)){
+        echo "<tr>";
+        echo "<td>". $row['name']."</td>";
+        echo "<td>". $row['phone']."</td>";
+        echo "<td>". $row['license']."</td>";
+        echo "<td>". $row['date']."</td>";
+        echo "<td>".$row['mechanic']."</td>";
+        echo "</tr>";
+    }
+   
+   ?>
+  
+  
+
+</table>
 // $query = mysql_query("SELECT * FROM client");
 
 
@@ -34,5 +71,5 @@ if (!$con)   /*  check if the connection was actually successful  */
 //    echo "$name<br>$address<br>$email<br>$license<br>$phone<br>$date<br>$engine<br>$mechanic";
 
 //    endwhile;
-
-?>
+</body>
+</html>
